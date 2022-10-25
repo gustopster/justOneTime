df635c836d0aa1b501cfd3878033b6ac08e915e5
gustopster <gustopster>
2022-10-24 23:17:35 -0300

    Finalização do prjeto.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-//informações do fixas adicionadas manualmente pelo App;
+//Não deixar a página Carregar (INICIO);
+function stopDefAction(evt) {
+  evt.preventDefault();
+}
+document.getElementById("botao").addEventListener(
+  'click', stopDefAction, false
+);
+//Não deixar a página Carregar (FINAL);
+//informações do fixas adicionadas manualmente pelo App (INICIO);
 const cordenadorSti = "Roberto Dias da Silva";
 const matriculaSti = "MAT: 809528";
 const enderecoSorocaba = [{
@@ -8,10 +16,13 @@ const enderecoSorocaba = [{
   "sala": "Informática",
   "andar": "Subsolo",
   "telefoneSorocaba": "(15) 2102-8423",
+  "bairro": "Alto da Boa Vista",
 }];
 const horario = new Date().toLocaleString();
-//App function em executão
-function justOneTime(numOb, numCo, idTec, idChamado, descMerc, patriMonio, quantiDade, vaLor) {
+//informações do fixas adicionadas manualmente pelo App (FINAL);
+//App function em executão (INICIO);
+function justOneTime(idTec,rgTec,numOb,numCo,idChamado,descMerc,patriMonio,serialNumber,descModelo,quantiDade,vaLor,userId,userMat,userTel,descProblema,descAtividade,descMauUso,fabricanteFornecedor) {
+  //Inicio Inputs Tabela
   let conteudoIdTec = document.getElementById('tecIt2b');
   conteudoIdTec.innerHTML = `Técnico IT2B: ${idTec}`;
   let conteudoNumOb = document.getElementById('correiosOb');
@@ -22,15 +33,17 @@ function justOneTime(numOb, numCo, idTec, idChamado, descMerc, patriMonio, quant
   let totalLinhas = tabela.rows.length;
   let linhas = tabela.insertRow(totalLinhas);
   let conteudoIdChamado = linhas.insertCell(0);
-  let conteudoDescMerc = linhas.insertCell(1);
-  let conteudoPatriMonio = linhas.insertCell(2);
-  let conteudoQuantiDade = linhas.insertCell(3);
-  let conteudoVaLor = linhas.insertCell(4);
   conteudoIdChamado.innerHTML = idChamado;
+  let conteudoDescMerc = linhas.insertCell(1);
   conteudoDescMerc.innerHTML = descMerc;
+  let conteudoPatriMonio = linhas.insertCell(2);
   conteudoPatriMonio.innerHTML = patriMonio;
+  let conteudoQuantiDade = linhas.insertCell(3);
   conteudoQuantiDade.innerHTML = quantiDade;
+  let conteudoVaLor = linhas.insertCell(4);
   conteudoVaLor.innerHTML = `R$ ${vaLor},00`;
+  // Final dos Inputs Tabela
+  // Inputs dos outros Textos e Condições para serem executados (INICIO)
   let stiDepartamento = document.getElementById("stiDepartamento").value;
   if (stiDepartamento === "") {
     stiDepartamento = `${document.getElementById("stiDepartamento").value}X.X`;
@@ -61,6 +74,198 @@ function justOneTime(numOb, numCo, idTec, idChamado, descMerc, patriMonio, quant
     sti.innerHTML = `<h3>STI ${stiDepartamento} - SERVIÇO DE APOIO AOS USUÁRIOS ${interiorOuCapital}</h3>`;
     let cordenador = document.getElementById("cordenador");
     cordenador.innerHTML = `<h3>Cordenador: ${cordenadorSti} – ${matriculaSti}</h3>`;
+    let ajustar = document.getElementById("tabelaRat");
+    ajustar.innerHTML =
+    `<div><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="4"><img src="css/it2bRat.png"></td>
+          <td rowspan="2"> Relatório de Atendimento Técnico - RAT</td>
+          <td>Nº CHAMADO CLIENTE: ${idChamado}</td>
+      </tr>
+      <tr>
+          <td>Nº CHAMADO IT2B: ${idChamado}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="3">CLIENTE: ${userId}</td>
+          <td rowspan="1"> TELEFONE / RAMAL: ${userTel}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="2">Endereço: ${enderecoSorocaba[0].rua}</td>
+          <td rowspan="1"> Bairro: ${enderecoSorocaba[0].bairro}</td>
+          <td rowspan="1"> Cidade: ${enderecoSorocaba[0].cidadeOrigem}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="1">FORNECEDOR/FABRICANTE:</td>
+          <td rowspan="1">TIPO:</td>
+          <td rowspan="1">PATRIMÔNIO:</td>
+          <td rowspan="1">SERIE:</td>
+          <td rowspan="1">MODELO:</td>
+      </tr>
+      <tr>
+          <td rowspan="1">${fabricanteFornecedor}</td>
+          <td rowspan="1">${descMerc}</td>
+          <td rowspan="1">${patriMonio}</td>
+          <td rowspan="1">${serialNumber}</td>
+          <td rowspan="1">${descModelo}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td>MOTIVO DO CHAMADO:</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td>ANORMALIDADE CONSTATADA:</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+      <tr>
+          <td>&nbsp;${descProblema}</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td>ATIVIDADES EXECUTADAS / OBSERVAÇÕES:</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+      <tr>
+          <td>&nbsp;${descAtividade}</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td>CASOS DE MAU USO (DESCREVER ABAIXO):</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+      <tr>
+          <td>&nbsp;${descMauUso}</td>
+      </tr>
+      <tr>
+          <td>&nbsp;</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="1">
+              CHAMADO FINALIZADO?S(&nbsp;)N(&nbsp;)
+              TENSÃO A/C:F/N
+              (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+              F/T
+              (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+              N/T
+              (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+          </td>
+      </tr>
+      <tr>
+          <td rowspan="1">
+              CONTADOR DE PÁGINAS - INICIAL:(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+              FINAL: (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+              FIRMWARE
+              VERS.(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+          </td>
+      </tr>
+      <tr>
+          <td rowspan="1">
+              EQUIPAMENTO OPERACIONAL: SIM(&nbsp;) NÃO(&nbsp;)
+              NÚMERO DO IP: (&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
+          </td>
+      </tr>
+      <tr>
+          <td rowspan="1">
+              NECESSÁRIO SUBSTITUIR PEÇAS: SIM(&nbsp;) NÃO(&nbsp;)
+          </td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="1">PN:</td>
+          <td rowspan="1">PN:</td>
+          <td rowspan="1">PN:</td>
+      </tr>
+      <tr>
+          <td rowspan="1">PN:</td>
+          <td rowspan="1">PN:</td>
+          <td rowspan="1">PN:</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="2">VISTO DO USUÁRIO:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td rowspan="2">DATA E HORA: ${horario}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="1">NOME DO USUÁRIO:</td>
+          <td rowspan="1">MATRÍCULA:</td>
+      </tr>
+      <tr>
+        <td>&nbsp;${userId}</td>
+        <td>&nbsp;${userMat}</td>
+      </tr>
+  </tbody>
+</table>
+<table id="" border="1" width="100%" class="">
+  <tbody>
+      <tr>
+          <td rowspan="1">NOME DO TÉCNICO IT2B: ${idTec}</td>
+          <td rowspan="1">RG DO TÉCNICO: ${rgTec}</td>
+          <td rowspan="2">VISTO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+      </tr>
+  </tbody>
+</table>
+</div>`;
   } else {
     let informarComarca = document.getElementById("informarComarca");
     informarComarca.innerHTML = `<p>!!!SELECIONE TODAS AS OPÇÕES ACIMA PARA CONTINUAR!!!</p>`;
@@ -73,46 +278,6 @@ function justOneTime(numOb, numCo, idTec, idChamado, descMerc, patriMonio, quant
     let cordenador = document.getElementById("cordenador");
     cordenador.innerHTML = `<h3>!!!SELECIONE TODAS AS OPÇÕES ACIMA PARA CONTINUAR!!!</h3>`;
   };
+  // Inputs dos outros Textos e Condições para serem executados (FINAL)
 };
-//Não deixar a página Carregar
-function stopDefAction(evt) {
-  evt.preventDefault();
-}
-document.getElementById("botao").addEventListener(
-  'click', stopDefAction, false
-);
-//anexar foto da assinatura
-const fileSelect = document.getElementById("fileSelect"),
-  fileElem = document.getElementById("fileElem"),
-  fileList = document.getElementById("fileList");
-fileSelect.addEventListener("click", (e) => {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
-fileElem.addEventListener("change", handleFiles, false);
-function handleFiles() {
-  if (!this.files.length) {
-    fileList.innerHTML = "<p>No files selected!</p>";
-  } else {
-    fileList.innerHTML = "";
-    const list = document.createElement("ul");
-    fileList.appendChild(list);
-    for (let i = 0; i < this.files.length; i++) {
-      const li = document.createElement("li");
-      list.appendChild(li);
-
-      const img = document.createElement("img");
-      img.src = URL.createObjectURL(this.files[i]);
-      img.height = 60;
-      img.onload = () => {
-        URL.revokeObjectURL(img.src);
-      }
-      li.appendChild(img);
-      const info = document.createElement("span");
-      info.innerHTML = ``;
-      li.appendChild(info);
-    }
-  }
-};
+//App function em executão (FINAL);
